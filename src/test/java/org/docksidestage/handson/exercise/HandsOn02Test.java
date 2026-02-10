0a412beacf3a801d1d5092f0b54184c3d789ccb5
a.kumoshita <a.kumoshita@bizreach.co.jp>
2026-02-10 15:51:56 +0900

    fear: テストコードの実装

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -1,9 +1,25 @@
 package org.docksidestage.handson.exercise;
 
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 /**
  * @author a.kumoshita
  */
 public class HandsOn02Test extends UnitContainerTestCase {
+
+    @Resource
+    private MemberBhv memberBhv;
+
+    public void test_existsTestData() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        int count = memberBhv.selectCount(cb -> {});
+
+        // ## Assert ##
+        assertTrue(count > 0);
+    }
 }
