da0381181fe8fb7a5e55b68a1be8f4e852de23de
a.kumoshita <a.kumoshita@bizreach.co.jp>
2026-02-10 14:51:54 +0900

    fear: 事前準備->HandsOn02Testを作成

---
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -0,0 +1,9 @@
+package org.docksidestage.handson.exercise;
+
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
+/**
+ * @author a.kumoshita
+ */
+public class HandsOn02Test extends UnitContainerTestCase {
+}
